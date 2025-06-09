e22fd258a46876e4e178a86b7fc7d7b9df6cce47
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-09 13:20:09 +0200

    feature Base One

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ export default function RootLayout() {
     return (
         <html lang="fr-FR">
             <head>
-                <meta name="robots" content="noindex, nofollow" />
+                <link rel="canonical" href="https://peur-de-la-conduite.fr" />
             </head>
             <head></head>
             <body>server</body>
