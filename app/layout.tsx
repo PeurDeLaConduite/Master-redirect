4c4072c43d7c86f3ccb7e5e76b3eea2aac8a3fe1
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-17 19:07:31 +0200

    feature HTTP2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,37 +7,38 @@ export default function RootLayout() {
                 <meta name="robots" content="index, follow" />
                 <link
                     rel="icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/logo.svg"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/logo.svg"
                     type="image/svg+xml"
                 />
                 <link
                     rel="icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/favicon-16x16.png"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/favicon-16x16.png"
                     sizes="16x16"
                     type="image/png"
                 />
                 <link
                     rel="icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/icons/favicon-32x32.png"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/favicon-32x32.png"
                     sizes="32x32"
                     type="image/png"
                 />
                 <link
                     rel="icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-192x192.png"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-192x192.png"
                     sizes="192x192"
                     type="image/png"
                 />
                 <link
                     rel="icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-512x512.png"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-512x512.png"
                     sizes="512x512"
                     type="image/png"
                 />
                 <link
                     rel="apple-touch-icon"
-                    href="https://s3.eu-west-3.amazonaws.com/assets.peur-de-la-conduite.fr/img/favicon/apple-touch-icon.png"
-                    sizes="180x180"type="image/png"
+                    href="https://assets.peur-de-la-conduite.fr/img/favicon/apple-touch-icon.png"
+                    sizes="180x180"
+                    type="image/png"
                 />
                 <link rel="canonical" href="https://peur-de-la-conduite.fr" />
             </head>
