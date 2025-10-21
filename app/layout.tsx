b5757e555e974d4ea91bbc11b73ce6502b3c009b
Jerem16 <jeremy.lemaignent@gmail.com>
2025-10-21 14:16:06 +0200

    start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,28 @@
-"use client";
+import type { Metadata } from "next";
+export const metadata: Metadata  = {
+    title: "Peur de la conduite",
+    description: "Coach amoxophobie, surmonter la peur de la conduite",
+    icons: {
+        icon: "/img/favicon/logo.svg",
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+    alternates: {
+        canonical: "https://peur-de-la-conduite.fr",
+    },
+};
 
-export default function RootLayout() {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
         <html lang="fr-FR">
-            <head>
-                <meta name="robots" content="index, follow" />
-                <link
-                    rel="icon"
-                    href="https://assets.peur-de-la-conduite.fr/img/favicon/logo.svg"
-                    type="image/svg+xml"
-                />
-                <link
-                    rel="icon"
-                    href="https://assets.peur-de-la-conduite.fr/img/favicon/logo.svg"
-                    sizes="16x16"
-                    type="image/svg+xml"
-                />
-                <link
-                    rel="icon"
-                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/favicon-32x32.png"
-                    sizes="32x32"
-                    type="image/png"
-                />
-                <link
-                    rel="icon"
-                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-192x192.png"
-                    sizes="192x192"
-                    type="image/png"
-                />
-                <link
-                    rel="icon"
-                    href="https://assets.peur-de-la-conduite.fr/img/favicon/icons/android-chrome-512x512.png"
-                    sizes="512x512"
-                    type="image/png"
-                />
-                <link rel="canonical" href="https://peur-de-la-conduite.fr" />
-            </head>
-            <body>server</body>
+            <head />
+            <body>{children}</body>
         </html>
     );
 }
