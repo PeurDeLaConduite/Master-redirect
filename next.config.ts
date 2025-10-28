3c5606f39be53d586a0fdc6aa73b412f0e669c9d
Jerem16 <jeremy.lemaignent@gmail.com>
2025-10-28 09:21:12 +0100

    start

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,51 @@
-// next.config.ts (extrait)
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    minimumCacheTTL: 60 * 60 * 24 * 365,
-  },
-  async headers() {
-    return [
-      {
-        source: "/img/:path*",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
-        ],
-      },
-      {
-        source: "/services/:path*",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
-        ],
-      },
-    ];
-  },
+    experimental: {},
+
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        // ignoreBuildErrors: true,
+    },
+
+    images: {
+        // augmente la mise en cache par défaut du loader next/image
+        minimumCacheTTL: 60 * 60 * 24 * 365, // 365 jours
+    },
+
+    async headers() {
+        return [
+            // Fichiers JS/CSS/JSON générés par Next (hashés)
+            //   {
+            //     source: "/_next/static/:path*",
+            //     headers: [
+            //       { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+            //     ],
+            //   },
+
+            // CSS “manuellement” dans /css
+            //   {
+            //     source: "/css/:path*",
+            //     headers: [
+            //       { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+            //     ],
+            //   },
+
+            // SVG, ICO, etc. dans /img et /services
+            {
+                source: "/img/:path*",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
